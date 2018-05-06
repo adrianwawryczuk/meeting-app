1069bb0d6f2da9cfce3ea3f9e14696d9e336821f
Adrian Wawryczuk <adrian.wawryczuk@avra.pl>
2018-05-06 22:07:37 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,103 @@
+initIndustries();
+
+initNumbers();
+
+function initNumbers() {
+    var options = {
+        useEasing: true,
+        useGrouping: true,
+        separator: ' ',
+    };
+
+    var numbers = [
+        { animation: new CountUp('eventsNumber', 0, 250, 0, 2.5, options), selector: 'eventsNumber', animated: false},
+        { animation: new CountUp('usersNumber', 0, 100000, 0, 1.5, options), selector: 'usersNumber', animated: false},
+        { animation: new CountUp('screenViewsNumber', 0, 6400000, 0, 1.5, options), selector: 'screenViewsNumber', animated: false},
+        { animation: new CountUp('timeUsageNumber', 0, 5, 0, 3.5, options), selector: 'timeUsageNumber', animated: false},
+        { animation: new CountUp('countriesNumber', 0, 31, 0, 2.5, options), selector: 'countriesNumber', animated: false},
+        { animation: new CountUp('continentsNumber', 0, 6, 0, 2.5, options), selector: 'continentsNumber', animated: false},
+    ];
+
+    window.addEventListener('scroll', function() {
+        for (var i = 0; i<numbers.length; i++) {
+            var number = numbers[i];
+
+            if (isNumbersInViewport(number.selector) && !number.animated) {
+                number.animation.start();
+                number.animated = true;
+            }
+        }
+    });
+
+    function isNumbersInViewport(elementId) {
+        var el = document.getElementById(elementId);
+
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+}
+
+function initIndustries() {
+    var classVisible = 'industries__info--visible';
+    var buttons = document.querySelectorAll('[data-show]');
+    var interval = createInterval();
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(element) {
+
+            if (interval) {
+                clearInterval(interval);
+            }
+
+            showIndustry(element.target, true);
+        });
+    });
+
+    function showIndustry(element) {
+        hideAllIndustries();
+
+        var dataShowName = element.attributes.getNamedItem('data-show').value;
+
+        document.querySelector('[data-showid="' + dataShowName + '"]')
+                .classList
+                .add(classVisible);
+    }
+
+    function hideAllIndustries() {
+        document.querySelectorAll('.' + classVisible)
+                .forEach(function(element) {
+                    element.classList.remove(classVisible);
+                });
+    }
+
+    function getAllIndustries() {
+        return document.querySelectorAll('[data-show]');
+    }
+
+    function createInterval() {
+        var index = 0;
+
+        showIndustry(getAllIndustries()[0]);
+
+        return setInterval(function() {
+
+            showIndustry(getAllIndustries()[index]);
+
+            incrementIndex();
+        }, 5000);
+
+        function incrementIndex() {
+            if (index < 3) {
+                index++;
+            } else {
+                index = 0;
+            }
+        }
+    }
+}
+
