89d4280c234745a29b87cce980ed9ddff60423b3
Adrian Wawryczuk <adrian.wawryczuk@avra.pl>
2018-08-22 00:23:13 +0200

    first features

--- src/features/features.js
+++ src/features/features.js
@@ -3,12 +3,21 @@ import $ from 'jquery';
 import 'owl.carousel';
 
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    $('.cms__owl').owlCarousel({
         center: true,
         items:2,
         loop:false,
         dots: true,
         margin:60,
         startPosition: 1
+    });
+
+    $('.features__phones').owlCarousel({
+        center: true,
+        items:1,
+        loop:false,
+        dots: false,
+        nav: false,
+        startPosition: 9
     });
 });
